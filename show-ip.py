06cd04cc019e5934368eff18d21e7a3aa642e739
Ken Graf <ken.graf@cs.unh.edu>
2021-01-14 07:56:57 -0500

    Update show-ip.py

--- show-ip.py
+++ show-ip.py
@@ -8,11 +8,11 @@ serverPort = 80
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
 
-        self.send_response(200)
+        self.send_response(2200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<html><head><title>SUCCESS</title></head>", "utf-8"))
         self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
         self.wfile.write(bytes("<p>This is an example web server %s.</p>" % socket.gethostbyname(socket.gethostname()), "utf-8"))
