982c062e8defa82d0244683aa5824fd27cf7cb39
Ken Graf <ken.graf@cs.unh.edu>
2025-01-06 21:57:03 -0500

    Update __init__.py

--- Lab4-Cloud-functions/Azure/code/return-uuid/__init__.py
+++ Lab4-Cloud-functions/Azure/code/return-uuid/__init__.py
@@ -6,4 +6,8 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    return func.HttpResponse("hi") # str(uuid.uuid4())
+    new_uuid = uuid.uuid4()
+    return func.HttpResponse(
+        f"Generated UUID: {new_uuid}",
+        status_code=200
+    )
