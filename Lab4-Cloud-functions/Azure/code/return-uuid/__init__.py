91e9aeed5a3075f74e9cd5884c70ecfaaa905aad
Ken Graf <ken.graf@cs.unh.edu>
2025-01-07 06:23:17 -0500

    Update __init__.py

--- Lab4-Cloud-functions/Azure/code/return-uuid/__init__.py
+++ Lab4-Cloud-functions/Azure/code/return-uuid/__init__.py
@@ -5,6 +5,9 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    # Check if the request is for favicon.ico
+    if req.url.endswith('/favicon.ico'):
+        return func.HttpResponse("", status_code=204) 
 
     generated_uuid = uuid.uuid4()
     
