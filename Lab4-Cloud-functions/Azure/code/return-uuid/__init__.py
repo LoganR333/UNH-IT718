359854d3804c11e9e1ac47dce85e5126543fa110
Ken Graf <ken.graf@cs.unh.edu>
2025-01-06 19:25:05 -0500

    Update __init__.py

--- Lab4-Cloud-functions/Azure/code/return-uuid/__init__.py
+++ Lab4-Cloud-functions/Azure/code/return-uuid/__init__.py
@@ -4,11 +4,4 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        return func.HttpResponse(
-            "Please pass a name on the query string or in the request body",
-            status_code=400
-        )
-
-    return func.HttpResponse(f"Hello, {name}!")
+    return func.HttpResponse(str(uuid.uuid4())
