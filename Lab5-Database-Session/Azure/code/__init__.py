9d431ef761c5efc9dce72efb42726b16f198606d
Ken Graf <ken.graf@cs.unh.edu>
2025-01-07 07:58:08 -0500

    Create __init__.py

---
+++ Lab5-Database-Session/Azure/code/__init__.py
@@ -0,0 +1,25 @@
+import json
+import uuid
+import logging
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    # Check if the request is for favicon.ico
+    if req.url.endswith('/favicon.ico'):
+        return func.HttpResponse("", status_code=204) 
+
+    generated_uuid = uuid.uuid4()
+    
+    # Create HTML response
+    html_content = f"<h1>Hello, World!</h1><p>Your session UUID: {generated_uuid}</p>"
+    
+    response = func.HttpResponse(
+        body=html_content,
+        status_code=200,
+        mimetype="text/html"
+        )
+    response.headers["Set-Cookie"] = f"test-id={generated_uuid}; Domain=return-uuid.azurewebsites.net; Path=/"
+
+    return response
+    
