fe351df1bdacfd92be7d3b4d4f444ed80c022739
Ken Graf <ken.graf@cs.unh.edu>
2025-01-08 11:04:57 -0500

    Update main.py

--- Lab4-Cloud-functions/GCP/main.py
+++ Lab4-Cloud-functions/GCP/main.py
@@ -7,30 +7,6 @@ import functions_framework
 db = firestore.Client()
 
 @functions_framework.http
-def cors_enabled_function(request):
-    # For more information about CORS and CORS preflight requests, see:
-    # https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
-
-    # Set CORS headers for the preflight request
-    if request.method == "OPTIONS":
-        # Allows GET requests from any origin with the Content-Type
-        # header and caches preflight response for an 3600s
-        headers = {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET",
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Access-Control-Max-Age": "3600",
-        }
-
-        return ("", 204, headers)
-
-    # Set CORS headers for the main request
-    headers = {"Access-Control-Allow-Origin": "*"}
-
-    return ("Hello World!", 200, headers)
-
-
-
 
 def handler(request):
     # Parse the request path
@@ -55,7 +31,7 @@ def handle_new(email):
 
     try:
         # Store the email and UUID in Firestore
-        db.collection('users').document(email).set({'uuid': new_uuid})
+        db.collection('return-uuid').document(email).set({'uuid': new_uuid})
     except Exception as e:
         return f"Error storing data: {str(e)}", 500
 
@@ -77,7 +53,7 @@ def handle_get(email, cookie):
 
     try:
         # Retrieve the record from Firestore
-        doc_ref = db.collection('users').document(email)
+        doc_ref = db.collection('return-uuid').document(email)
         doc = doc_ref.get()
 
         if not doc.exists:
