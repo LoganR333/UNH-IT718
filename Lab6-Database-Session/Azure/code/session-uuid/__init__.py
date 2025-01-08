cfc365d24b7203b2a0165ce0b97572c097c020f3
Ken Graf <ken.graf@cs.unh.edu>
2025-01-08 06:18:57 -0500

    Delete Lab6-Database-Session/Azure/code/session-uuid directory

--- Lab6-Database-Session/Azure/code/session-uuid/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-import azure.functions as func
-from azure.data.tables import TableServiceClient
-import uuid
-import os
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("test",status_code=200)
