84dafe82767df6d1f6158d09cf2d7facc2b8d113
Ken Graf <ken.graf@cs.unh.edu>
2025-01-10 15:21:22 -0500

    Create tests.sh

---
+++ Lab7-OpenId-Connect/tests/tests.sh
@@ -0,0 +1,8 @@
+# Various tests as componeents deploy
+curl -X POST https://<api-endpoint>/trigger \
+  -H "Content-Type: application/json" \
+  -d '{"email": "user@example.com", "action": "create"}'
+
+curl -X POST https://<api-endpoint>/trigger \
+  -H "Content-Type: application/json" \
+  -d '{"email": "user@example.com", "action": "read"}'
