819fcee3c7a2f8df841f4319340305892cf499c7
Ken Graf <ken.graf@cs.unh.edu>
2025-01-10 17:08:27 -0500

    Update deploy.sh

--- Lab7-OpenId-Connect/deploy/deploy.sh
+++ Lab7-OpenId-Connect/deploy/deploy.sh
@@ -12,21 +12,16 @@ S3BUCKET=$STACK_NAME-$(tr -dc a-f0-9 </dev/urandom | head -c 10)
 sed -ri "s/bucket-random/${S3BUCKET}/" parameters.json
 sed -ri "s/IT718Lab7-[0-9a-f]*/${S3BUCKET}/" parameters.json
 
-S3BUCKET=$STACK_NAME
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-echo "Creating stack..."
-STACK_ID=$()
-echo "STACK_ID=$STACK_ID"
+echo "Creating stack... $STACK_NAME"
 
 # Ideally the CloudFormation stacks would be combined into one.
 # This appoach is allow students to alter steps as needed
 
-STACK_ID="$STACK_NAME-storage"
-aws cloudformation create-stack --stack-name ${STACK_ID} --template-body file://${STACK_ID}.json --parameters file://parameters.json --tags file://tags.json --output text
-echo "Waiting on ${STACK_ID} create completion..."
-aws cloudformation wait stack-create-complete --stack-name ${STACK_ID}
-aws cloudformation describe-stacks --stack-name ${STACK_ID} | jq .Stacks[0].Outputs
+STACK_NAME="$STACK_NAME-storage"
+aws cloudformation create-stack --stack-name ${STACK_NAME} --template-body file://${STACK_NAME}.json --parameters file://parameters.json --tags file://tags.json --output text
+echo "Waiting on ${STACK_NAME} create completion..."
+aws cloudformation wait stack-create-complete --stack-name ${STACK_NAME}
+aws cloudformation describe-stacks --stack-name ${STACK_NAME} | jq .Stacks[0].Outputs
 exit
 
 # Package and upload the lambda function
@@ -48,6 +43,6 @@ aws cloudformation describe-stacks --stack-name "IT718Lab7" --query "Stacks[*].{
 VAR=$(aws cloudformation list-exports --query "Exports[?contains(Name,'IT718Lab7-ApiEndpoint')].[Value]" --output text)
 sed -ri "s^(invokeUrl: )('.*')^\1'${VAR}'^i" website/scripts/config.js
 
-echo "Waiting on ${STACK_ID} create completion..."
-aws cloudformation wait stack-create-complete --stack-name ${STACK_ID}
-aws cloudformation describe-stacks --stack-name ${STACK_ID} | jq .Stacks[0].Outputs
+echo "Waiting on ${STACK_NAME} create completion..."
+aws cloudformation wait stack-create-complete --stack-name ${STACK_NAME}
+aws cloudformation describe-stacks --stack-name ${STACK_NAME} | jq .Stacks[0].Outputs
