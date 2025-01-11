b1ed4824365dea22088aa89c9148d524e914807a
Ken Graf <ken.graf@cs.unh.edu>
2025-01-11 13:11:21 -0500

    Update deploy.sh

--- Lab7-OpenId-Connect/deploy/deploy.sh
+++ Lab7-OpenId-Connect/deploy/deploy.sh
@@ -1,45 +1,71 @@
 #!/bin/bash
 
-STACK_NAME=$1
-
 if [ -z "$1" ]
   then
-    echo "No STACK_NAME argument supplied"
+    echo "No DEPLOY_NAME argument supplied"
     exit 1
 fi
 
-S3BUCKET=$STACK_NAME-$(tr -dc a-f0-9 </dev/urandom | head -c 10)
-sed -ri "s/bucket-random/${S3BUCKET}/" parameters.json
-sed -ri "s/it718lab7-[0-9a-f]*/${S3BUCKET}/" parameters.json
-
-echo "Creating stack... $STACK_NAME"
+DeployName=$1
+DomainName=$DeployName.kengraf.com
+S3BUCKET=$DeployName  # Needs to be globally unique
 
 # Ideally the CloudFormation stacks would be combined into one.
 # This appoach is allow students to alter steps as needed
 
 STACK_NAME="$STACK_NAME-storage"
-aws cloudformation create-stack --stack-name ${STACK_NAME} --template-body file://${STACK_NAME}.json --parameters file://parameters.json --tags file://tags.json --output text
+echo "Creating stack... $STACK_NAME"
+aws cloudformation deploy --stack-name ${STACK_NAME} \
+  --template-file file://${STACK_NAME}.json \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+      S3bucketName=${S3BUCKET} \
+      DeployName=${DeployName} \
+  --tags file://tags.json --output text
 echo "Waiting on ${STACK_NAME} create completion..."
+aws cloudformation wait stack-create-complete --stack-name ${STACK_NAME}
+aws cloudformation describe-stacks --stack-name ${STACK_NAME} | jq .Stacks[0].Outputs
+
+echo "Deploying backend components (apigatewayv2, lambda, dynamodb)"
+STACK_NAME="$DeployName-backend"
+aws cloudformation deploy --stack-name ${STACK_NAME} \
+  --template-file file://${STACK_NAME}.json \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+      DeployName=${DeployName} \
+  --tags file://tags.json --output text
+echo "Waiting on ${STACK_NAME} create completion..."
+aws cloudformation wait stack-create-complete --stack-name ${STACK_NAME}
+aws cloudformation describe-stacks --stack-name ${STACK_NAME} | jq .Stacks[0].Outputs
+
+echo "Deploy a CloudFront distribution"
+CertificateArn="arn:aws:acm:us-east-2:788715698479:certificate/f5a199d0-7175-4992-bc7c-8ed2ad71541b"
+DomainName="it718lab7.kengraf.com"
+HostedZoneId="Z04154431JUEDZVN0IZ8F"
+# Uncomment the next line if you do NOT have a Route53 hosted zone, previous settings will be ignored
+#HostedZoneId=""  
+STACK_NAME="$DeployName-distribution"
+aws cloudformation deploy --stack-name ${STACK_NAME} \
+  --template-file file://${STACK_NAME}.json \
+  --parameter-overrides \
+      HostedZoneId=${HostedZoneId} \
+      DomainName=${DomainName} \
+      CertificateArn=${CertificateArn} \
+  --capabilities CAPABILITY_NAMED_IAM
 aws cloudformation wait stack-create-complete --stack-name ${STACK_NAME}
 aws cloudformation describe-stacks --stack-name ${STACK_NAME} | jq .Stacks[0].Outputs
 
 echo "Packaging and uploading the lambda function"
 cd lambda
 zip function.zip verifyToken.py
-aws s3 cp function.zip s3://${S3BUCKET}/function.zip
+aws s3 cp function.zip s3://${S3BUCKET}
 cd ..
 
 echo "Uploading website content"
 cd website
-aws s3 sync . s3://IT718Lab7
+aws s3 sync . s3://${S3BUCKET}
 cd ..
 
-echo "Deploying backend components (apigatewayv2, lambda, dynamodb)"
-STACK_NAME="$STACK_NAME-backend"
-aws cloudformation create-stack --stack-name ${STACK_NAME}a --template-body file://${STACK_NAME}.json --capabilities CAPABILITY_NAMED_IAM --parameters file://parameters.json --tags file://tags.json --output text
-echo "Waiting on ${STACK_NAME} create completion..."
-aws cloudformation wait stack-create-complete --stack-name ${STACK_NAME}
-aws cloudformation describe-stacks --stack-name ${STACK_NAME} | jq .Stacks[0].Outputs
 
 aws cloudformation describe-stacks --stack-name ${STACK_NAME} --query "Stacks[*].{StackId: StackId, StackName: StackName}
 
