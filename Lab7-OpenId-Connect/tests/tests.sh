ad2faa4091dac81d5a37dd2be958e35be80a7cf4
Ken Graf <ken.graf@cs.unh.edu>
2025-01-14 18:56:13 -0500

    Update tests.sh

--- Lab7-OpenId-Connect/tests/tests.sh
+++ Lab7-OpenId-Connect/tests/tests.sh
@@ -5,11 +5,11 @@ curl -X POST https://it718lab7.kengraf.com/v1/verifyToken \
 
 curl -X POST https://d3obdogsmpp9uy.cloudfront.net/v1/verifyToken \
   -H "Content-Type: application/json" \
-  -d '{"email": "user@example.com", "action": "create"}'
+  -d '{"idToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"}'
 
 curl -X POST https://6863p36t83.execute-api.us-east-2.amazonaws.com/v1/verifyToken \
   -H "Content-Type: application/json" \
-  -d '{"id_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"}'
+  -d '{"idToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"}'
 
   {
   "httpMethod": "POST",
